e140e1e4f4fd6fda9f823d598db730fcc4a4560e
Vidoje Zeljic <vidoje.zeljic@fioneer.com>
2023-12-17 14:41:30 +0100

    basic validations

--- src/main/java/com/vidoje/validation/ValidationController.java
+++ src/main/java/com/vidoje/validation/ValidationController.java
@@ -1,14 +1,14 @@
 package com.vidoje.validation;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 public class ValidationController {
 
@@ -23,7 +23,7 @@ public class ValidationController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void post(ValidationRequestDto requestDto) {
+    public void post(@Valid @RequestBody ValidationRequestDto requestDto) {
         validationService.post(requestDto);
     }
 }
--- src/main/java/com/vidoje/validation/ValidationRequestDto.java
+++ src/main/java/com/vidoje/validation/ValidationRequestDto.java
@@ -1,10 +1,18 @@
 package com.vidoje.validation;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ValidationRequestDto {
     private String name;
+    @Min(1)
     private int min;
+    @Max(10)
     private int max;
 }
