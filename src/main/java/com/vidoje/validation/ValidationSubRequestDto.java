f89f6ae8ba9f6f03aa76da5ad5bc05c274ad5133
Vidoje Zeljic <vidoje.zeljic@fioneer.com>
2023-12-18 11:35:51 +0100

    extend dto with sub-class

--- src/main/java/com/vidoje/validation/ValidationSubRequestDto.java
+++ src/main/java/com/vidoje/validation/ValidationSubRequestDto.java
@@ -1,6 +1,5 @@
 package com.vidoje.validation;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,13 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@MinMaxCreate(groups = OnCreate.class)
-@MinMaxUpdate(groups = OnUpdate.class)
-public class ValidationRequestDto {
-    private String name;
-    private int min;
+public class ValidationSubRequestDto {
     @Min(value = 5, groups = OnCreate.class)
     private int max;
-    @Valid
-    private ValidationSubRequestDto sub;
 }
